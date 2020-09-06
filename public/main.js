a0f5e4e84ed2597ff07c828c6a184b7ebf726ab5
Trinh Duy An <luongduongduc4@gmail.com>
2020-09-06 22:35:27 +0700

    project

--- public/main.js
+++ public/main.js
@@ -9,18 +9,18 @@ $.ajax({
 });
 function hienThiuser (arr) {
     for (let i = 0 ; i < arr.length ; i ++) {
-        $("tbody").append(`<tr class="text-center bg-info">
+        $("tbody").append(`<tr class="text-center bg-info"  id="tr-${arr[i].id}">
         <td>${arr[i].name}</td>
         <td>${arr[i].birthday}</td>
         <td>${arr[i].phone}</td>
         <td>${arr[i].email}</td>
         <td><a href="edit.html?id=${arr[i].id}" class="text-warning">Chỉnh sửa <i class="fa fa-edit"></i></a> | <a
-                href="javascript:void(0)" class="text-danger" onclick="deleteUser(1, this)"><i
+                href="javascript:void(0)" class="text-danger" onclick="confirmDelete(${arr[i].id})"><i
                     class="fa fa-trash-alt"></i> Xóa</a></td>
     </tr>`)
     }
 }
-function createUser() {
+function createUser(id) {
     $.ajax({
         type: "POST",
         url: "https://tda123.herokuapp.com/users",
@@ -58,7 +58,21 @@ let id = window.location.href.slice(vt + 1);
 upDateuser(id);
 }
 
-
-
-
-
+function confirmDelete(id) {
+    $("#exampleModal").modal("show");
+    $('#confirm-delete').click(function () {
+        deleteUser(id);
+        $("#exampleModal").modal("hide");
+    });
+  }
+  
+function deleteUser(id) {
+    $.ajax({
+        type: "DELETE",
+        url: `https://tda123.herokuapp.com/users/${id}`,
+        dataType: "json",
+        success: function () {
+            $(`#tr-${id}`).remove();
+        },
+    });
+}
